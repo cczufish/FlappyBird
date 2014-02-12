f4a1ca0f37500bddcea4e5698ff5373cddb5f971
yushuhui <yushuhui@91waijiao.com>
2014-02-12 17:28:28 +0800

    flappy bird cocos2dx 3.0

---
+++ Classes/resource.h
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////
+static const char bird_hero[] = "bird_hero.png";
+static const char bird_hero2[] = "bird_hero2.png";
+static const char bird_hero3[] = "bird_hero3.png";
+static const char bird_logo[] = "bird_logo.png";
+static const char bird_bg[] = "bird_bg.png";
+static const char bird_obstacle_up[] = "obstacle_up.png";
+static const char bird_obstacle_down[] = "obstacle_down.png";
+static const char bird_start_btn[] = "bird_start_btn.png";
+static const char bird_start_btn_pressed[] = "brid_start_btn_pressed.png";
+static const char bird_gameover[] = "bird_gameover.png";
+
+static const int TAG_LOGO = 1000;
+static const int TAG_START_BTN= 1001;
+static const int TAG_HERO = 1002; 
+static const int TAG_OVER = 1003;
+static const int TAG_SCORE = 1004;
+
+static const int GAME_STATUS_START = 10;
+static const int GAME_STATUS_PLAYING = 20;
+static const int GAME_STATUS_GAME_OVER = 30;
+static const int GAME_STATUS_RESTART = 40;
